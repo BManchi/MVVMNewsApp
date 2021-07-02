401863bc46de2f61d5ab75712fcfc023e2df022d
Bruno <b.manchinelli@gmail.com>
2021-07-02 10:00:32 -0300

    Progress

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+interface Constants {
+    companion object {
+        const val API_KEY = "62f8194be4c546bb86daaa36050a2883"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
